98dd2496d87be8e9829712344502f72b662fb2c7
Mustafa-Moghazy <73305128+Mustafa-Moghazy@users.noreply.github.com>
2024-04-16 00:13:45 +0200

    add CrossOrigin anotation

--- coupon/coupon/src/main/java/com/example/coupon/controller/CouponController.java
+++ coupon/coupon/src/main/java/com/example/coupon/controller/CouponController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api")
+@git(origins = "http://localhost:4200")
 public class CouponController {
     @Autowired
     private CouponService couponService;
